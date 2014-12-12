9a8e0163ff8b0d5067141917582cb1d102adeae1
YaroslavTir <yaroslavTir@gmail.com>
2014-12-12 14:47:47 +0300

    maven deploy

---
+++ deploy/src/main/java/DeployMeToTheRepo.java
@@ -0,0 +1,10 @@
+/**
+ * @author YMolodkov
+ * @since 16.0
+ */
+public class DeployMeToTheRepo {
+    public static void main(String[] args) {
+        System.out.println("some words");
+    }
+
+}
